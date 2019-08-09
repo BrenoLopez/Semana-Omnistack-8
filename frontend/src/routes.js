bf5c7c6236c74a0c9a68de7fb99f6f45411c2405
Breno Lopes <breno_lopes_10@yahoo.com.br>
2019-08-08 23:28:43 -0300

    frontend reactjs like e dislike

---
+++ frontend/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={Login}/>
+            <Route exact path='/dev/:id' component={Main}/>
+        </Switch>
+    </BrowserRouter>
+  );
+}
+ 
